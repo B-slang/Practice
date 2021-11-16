bc31b258aac87456811f2081266a6481a2873275
B-slang <entr.brijesh@gmail.com>
2021-11-16 14:22:42 +0530

    Done

---
+++ typefunctions/anonymous.js
@@ -0,0 +1,17 @@
+let show = function (x,y) {
+    return x+y
+}
+
+console.log(show(5,6))
+
+setTimeout(function () {
+    
+    let x=5;
+    let y = 5
+    console.log(x+y)
+    return x+y
+
+}, 3000)
+
+
+
