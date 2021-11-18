e4dd8a174ab403a7f02be0592711b7a00686ad55
B-slang <entr.brijesh@gmail.com>
2021-11-18 18:01:49 +0530

    timezone API

--- Express/timeroutes.js
+++ Express/timeroutes.js
@@ -1,14 +1,14 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
+// const app = express()
 
-const port = 4001
+// const port = 4001
 
-app.get('/time_zone', (req, res, next)=>{
-    res.send("TimeZone")
-})
+// app.get('/time_zone', (req, res, next)=>{
+//     res.send("TimeZone")
+// })
 
 
-app.listen(port, ()=>{
-    console.log("running server")
-})
+// app.listen(port, ()=>{
+//     console.log("running server")
+// })
