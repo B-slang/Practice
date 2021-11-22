e70bfc5dc6b2d07dac8e667a473bd58b5a0b34b6
Major <navikbipin@gmail.com>
2021-11-22 18:01:54 +0530

    error solved in sequelize post

---
+++ CRUD/setup.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('cruddb', 'postgres', 'root', {
+    host:'localhost',
+    dialect:'postgresql'
+})
+
+sequelize.authenticate().then(()=>{
+    console.log("connected");
+}).catch(Error=>{
+    console.log("error", Error );
+})
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize= sequelize;
+
+db.products= require('./models/product')(sequelize, Sequelize.DataTypes)
+
+db.sequelize.sync().then(()=>{
+    console.log("Yes Synced")
+})
