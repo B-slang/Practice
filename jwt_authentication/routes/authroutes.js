4858f6e7caa3c8c81330bc628289f0fce681252c
B-slang <entr.brijesh@gmail.com>
2021-11-26 17:58:35 +0530

    routes for auth and userauth

---
+++ jwt_authentication/routes/authroutes.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const router = router.express()
+const {signupauth,loginauth } = require('../controllers/authcontroller')
+
+router.get('/auth/signup', )    
+router.post('/auth/login')
+
+
+module.exports = router;
