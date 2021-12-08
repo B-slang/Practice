b72755c57016ac1585a2d3942e00c53544876f53
B-slang <entr.brijesh@gmail.com>
2021-12-08 18:27:29 +0530

    getting data from postman but not from ejs

--- nodemailer/app.js
+++ nodemailer/app.js
@@ -1,7 +1,7 @@
-var nodemailer = require('nodemailer');
+
 const express = require('express')
 const bodyParser = require('body-parser');
-const fs = require('fs')
+
 
 const {sendMail } = require('./sentemail')
 
@@ -11,14 +11,19 @@ const app = express()
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('views/index.ejs'))
+
+// app.use(express.static("views"));
+
+// app.use(express.static('views/index.ejs'))
+
+
 app.use(bodyParser.json())
 
 app.set('view engine', 'ejs');
 
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname + 'views/index.ejs')
+    res.render('index')
 })
 
 app.post('/sendemail',sendMail )
