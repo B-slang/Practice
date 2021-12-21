d2ffb9eee876dc88c13a8f1f7c68935194da0278
B-slang <entr.brijesh@gmail.com>
2021-12-21 18:51:38 +0530

    microqueueTask error solved

--- rediscrud/server.js
+++ rediscrud/server.js
@@ -1,28 +1,107 @@
-const { default: axios } = require('axios')
-const express = require('express')
-const app = express()
-const redis = require('redis')
+const express = require('express');
+const redis = require('redis');
+// added axios as HTTP client
+const app = express();
+const axios = require('axios');
+// const redis = require('redis');
+const client = redis.createClient();
+
+// const  = redis.createClient();
+// await redisClient.connect()
+(async () => {
+  
+  client.on('error', (err) => console.log('Redis Client Error', err));
+  
+  await client.connect();
+  console.log("+++++++++++++++++++++++++++++++++++++");
+})()
 
 
-const client = redis.createClient()
+
+// const client = redis.createClient({
+//     host: '127.0.0.1',
+//     port: 6379
+   
+// });
+
+// client.on('error', err => {
+//     console.log('Error ' + err);
+// });
 
 
-const default_time  =3600
 
-app.get('/', (req,res)=>{
-    res.send("redis crud")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+// added Fake API
+const MOCK_API = "https://jsonplaceholder.typicode.com/users/";
+
+//API Call
+
+app.get('/cache/user/:email', async (req, res) => {
+  const email = req.params.email;
+
+  try{
+    await client.get(email, async (err, response) => {
+      console.log(response);
+      console.log("________");
+      if(response) {
+        console.log("User successfully retrieved from cache");
+        res.status(200).send(JSON.parse(response));
+      } else {
+        const response = await axios.get(`${MOCK_API}?email=${email}`);
+        const user = response.data;
+        await client.setex(email, 600, JSON.stringify(user));
+        console.log("User successfully retrieved from the API");
+        res.status(200).send(user);
+      }
+    })
+  }catch(err) {
+    res.status(500).send({ error: err.message });
+  }
 })
 
-app.get('/photos', async(req,res)=>{
-    const data = await axios.get("https://jsonplaceholder.typicode.com/photos", {params:{albumId}})
-    
-    client.setEx("photos", default_time, JSON.stringify(data))
-    res.json(data)
-})
-app.get('/photos/:id', async(req,res)=>{
-    const data = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`)
-    res.json(data)
-})
 
 
 
@@ -33,14 +112,20 @@ app.get('/photos/:id', async(req,res)=>{
 
 
 
+// app.get('/user/:email', async (req, res) => {
+//   const email = req.params.email;
+
+//   try {
+//     const response = await axios.get(`${MOCK_API}?email=${email}`);
+//     const user = response.data
+//     console.log("User successfully retrieved from the API");
+//     res.status(200).send(user);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// })
 
 
-
-
-
-
-
-
-app.listen(3001, ()=>{
-    console.log("server up");
-})
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
